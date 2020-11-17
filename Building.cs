bc2511cab3054714cb90f0068b5a4f170bd1898d
Lacey Walker <bluelarksong@gmail.com>
2020-11-17 11:59:26 -0600

    following along in exercise, saving progress

--- Building.cs
+++ Building.cs
@@ -1,4 +1,43 @@
 namespace Planner
 {
-    // Define class here
+    public class Building
+    {
+        private string _designer { get; set; }
+        private DateTime _dateConstructed { get; set; }
+        private string _address { get; set; }
+        private string _owner { get; set; }
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string buyer)
+        {
+            _owner = buyer;
+        }
+
+        public void Design(string designer)
+        {
+            _designer = designer;
+        }
+    }
+
 }
