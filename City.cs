769a0d6895ae5f10f5a53f29ccfe0acf9b78dadb
Lacey Walker <bluelarksong@gmail.com>
2020-11-17 15:24:10 -0600

    started on Urban Planner2, setting up cities

---
+++ City.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Planner
+{
+    public class City
+    {
+        static void Main(string[] args)
+        {
+            City megalopolis = new City();
+
+            // foreach (Building building in megalopolis.Buildings) {
+            Console.WriteLine("...");
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -11,31 +11,31 @@ namespace Planner
             AshwoodModel.Depth = 300;
             AshwoodModel.Stories = 13;
             AshwoodModel.Construct();
-            AshwoodModel.Purchase("Faith");
-            AshwoodModel.Design("Lacey");
+            AshwoodModel.Purchase("Faith Magras");
+            AshwoodModel.Design("Lacey Walker");
 
             Building PinecrestModel = new Building("P Sherman 42");
             PinecrestModel.Width = 500;
             PinecrestModel.Depth = 600;
             PinecrestModel.Stories = 13;
             PinecrestModel.Construct();
-            PinecrestModel.Purchase("Lacey");
-            PinecrestModel.Design("Faith");
+            PinecrestModel.Purchase("Leah Robinson");
+            PinecrestModel.Design("Andrew Herring");
 
             Building BloomsdaleModel = new Building("555 Green Acers");
             BloomsdaleModel.Width = 1200;
             BloomsdaleModel.Depth = 800;
             BloomsdaleModel.Stories = 13;
             BloomsdaleModel.Construct();
-            BloomsdaleModel.Purchase("St. Stumpy");
-            BloomsdaleModel.Design("Isabelle");
+            BloomsdaleModel.Purchase("Chef Gumi");
+            BloomsdaleModel.Design("Old Joe");
 
-            Console.WriteLine($"Address: {AshwoodModel.GetAddress()}");
-            Console.WriteLine("---------------");
-            Console.WriteLine($"Designed by {AshwoodModel.GetDesigners()}");
-            Console.WriteLine($"Constructed on {AshwoodModel.GetDate()}");
-            Console.WriteLine($"Owned by {AshwoodModel.GetOwner()}");
-            Console.WriteLine($"{AshwoodModel.Volume} cubic meters of space");
+            // Console.WriteLine($"Address: {AshwoodModel.GetAddress()}");
+            // Console.WriteLine("---------------");
+            // Console.WriteLine($"Designed by {AshwoodModel.GetDesigners()}");
+            // Console.WriteLine($"Constructed on {AshwoodModel.GetDate()}");
+            // Console.WriteLine($"Owned by {AshwoodModel.GetOwner()}");
+            // Console.WriteLine($"{AshwoodModel.Volume} cubic meters of space");
 
         }
     }
